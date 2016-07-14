1f1556911006453ba2feeec7cc04ca07e49e4c8d
maltejk <malte@maltejk.de>
2016-07-14 16:19:16 +0200

    initial. deleted unnecessary files, made it my own(tm)

--- init.rb
+++ init.rb
@@ -1,15 +1,12 @@
 require 'redmine'
-require 'bootstrap_hooks'
-require 'application_helper_patch'
-require 'settings_helper_patch'
 
-Redmine::Plugin.register :redmine_bootstrap do
-  name 'Redmine Bootstrap theme plugin'
-  author 'Reuben Mallaby'
-  description 'This is a plugin modifying Redmine to use a Bootstrap theme'
-  version '0.0.3'
-  url 'http://mallaby.me/projects/redmine_bootstrap'
-  author_url 'http://mallaby.me'
+Redmine::Plugin.register :redmine_bluemine_theme do
+  name 'Redmine bluemine theme plugin'
+  author 'Malte Jan Kaffenberger'
+  description 'This is a plugin modifying Redmine to look better in a discrete way.'
+  version '0.0.1'
+  url 'https://github.com/maltejk/redmine_bluemine_theme'
+  author_url 'https://maltejk.de'
    
   requires_redmine :version_or_higher => '2.1.0'
 end
